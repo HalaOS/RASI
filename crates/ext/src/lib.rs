c6882d68c4fffdd628c8e4256e9ffa57025ec468
yayanyang <yayanyang@gmail.com>
2024-03-05 23:52:18 +0800

    daily commit

--- crates/ext/src/lib.rs
+++ crates/ext/src/lib.rs
@@ -1,6 +1,14 @@
 //! Extend features or experimental features that are useful for asynchronous programming.
 //!
 
+#[cfg(feature = "batching")]
+#[cfg_attr(docsrs, doc(cfg(feature = "batching")))]
 pub mod batching;
+
+#[cfg(feature = "event_map")]
+#[cfg_attr(docsrs, doc(cfg(feature = "event_map")))]
 pub mod event_map;
+
+#[cfg(feature = "queue")]
+#[cfg_attr(docsrs, doc(cfg(feature = "queue")))]
 pub mod queue;
--- crates/ext/src/queue.rs
+++ crates/ext/src/queue.rs
@@ -11,9 +11,6 @@ pub struct Queue<T> {
     slots: DashMap<usize, T>,
 }
 
-unsafe impl<T: Send> Send for Queue<T> {}
-unsafe impl<T: Send> Sync for Queue<T> {}
-
 impl<T> Default for Queue<T> {
     fn default() -> Self {
         Self {
