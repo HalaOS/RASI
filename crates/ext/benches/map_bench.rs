0eb8859b9c3da7cc9aefbb608acc66df1b1339f8
yayanyang <yayanyang@gmail.com>
2024-03-06 11:00:12 +0800

    ext: update locker+map benchmark

--- crates/ext/benches/map_bench.rs
+++ crates/ext/benches/map_bench.rs
@@ -10,7 +10,7 @@ fn main() {
     divan::main();
 }
 
-#[bench(threads)]
+#[bench(threads, sample_size = 1000)]
 fn bench_park_mutex_map(bencher: Bencher) {
     let map = parking_lot::Mutex::new(HashMap::<usize, usize>::new());
 
@@ -24,7 +24,21 @@ fn bench_park_mutex_map(bencher: Bencher) {
     })
 }
 
-#[bench(threads)]
+#[bench(threads, sample_size = 1000)]
+fn bench_std_mutex_map(bencher: Bencher) {
+    let map = std::sync::Mutex::new(HashMap::<usize, usize>::new());
+
+    let counter = AtomicUsize::new(0);
+
+    bencher.bench(|| {
+        let id = counter.fetch_add(1, Ordering::Relaxed);
+
+        map.lock().unwrap().insert(id, id);
+        map.lock().unwrap().remove(&id);
+    })
+}
+
+#[bench(threads, sample_size = 1000)]
 fn bench_dash_map(bencher: Bencher) {
     let map = DashMap::<usize, usize>::new();
 
