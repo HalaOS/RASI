47f34236229cdc8b8eb14f1b32e3c9a32d691c2b
yayanyang <yayanyang@gmail.com>
2024-03-25 16:54:04 +0800

    update

--- crates/rasi/src/net.rs
+++ crates/rasi/src/net.rs
@@ -190,13 +190,12 @@ pub struct TcpStream {
 
 impl Debug for TcpStream {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("TcpStream")
-            .field("socket", &self.sys_socket)
-            .field(
-                "syscall",
-                &format!("{:?}", self.syscall as *const dyn Network),
-            )
-            .finish()
+        write!(
+            f,
+            "TcpStream {:?} => {:?}",
+            self.local_addr(),
+            self.peer_addr()
+        )
     }
 }
 
