e69f0f3ffe27929b0bcfe16e8f51546e019825b4
yayanyang <yayanyang@gmail.com>
2024-03-26 12:26:38 +0800

    rasi: add block_on fn

--- crates/rasi/src/executor.rs
+++ crates/rasi/src/executor.rs
@@ -3,7 +3,7 @@
 //! This module is similar to std::thread, except it uses asynchronous tasks in place of threads.
 
 use futures::Future;
-use rasi_syscall::{global_executor, syscall_spawn};
+use rasi_syscall::{global_executor, syscall_block_on, syscall_spawn};
 
 /// Use global register [syscall](rasi_syscall::Executor) to spawn a task
 /// that polls the given future with output () to completion.
@@ -13,4 +13,15 @@ where
     Fut: Future<Output = ()> + Send + 'static,
 {
     syscall_spawn(global_executor(), fut)
+}
+
+/// Use global register [syscall](rasi_syscall::Executor) to run a future to completion on the current thread.
+///
+/// This function will block the caller until the given future has completed.
+pub fn block_on<Fut, R>(fut: Fut) -> R
+where
+    Fut: Future<Output = R> + Send + 'static,
+    R: Send + 'static,
+{
+    syscall_block_on(global_executor(), fut)
 }
