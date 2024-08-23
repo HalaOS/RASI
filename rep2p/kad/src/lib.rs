c2b1f39e4a4c0d3c39aee7343f8dc2bc38b67547
yayanyang <yayanyang@gmail.com>
2024-08-23 12:21:35 +0800

    update

--- rep2p/kad/src/lib.rs
+++ rep2p/kad/src/lib.rs
@@ -1,15 +1,5 @@
-pub mod kbucket;
-pub mod primitives;
-pub mod route_table;
-
-mod switch;
-pub use switch::*;
-
 #[allow(renamed_and_removed_lints)]
 mod proto;
 
-mod rpc;
-
-pub mod errors;
-
-mod routing;
+mod errors;
+pub use errors::*;
