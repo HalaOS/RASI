04ecfe7d88a453875f2acb160832d3b1db390aba
yayanyang <yayanyang@gmail.com>
2024-08-26 20:32:20 +0800

    dnsaddr:  init

---
+++ rep2p/dnsaddr/src/lib.rs
@@ -0,0 +1,29 @@
+use std::io::Result;
+
+use async_trait::async_trait;
+use rep2p::multiaddr::Multiaddr;
+use rep2p::transport::syscall::DriverTransport;
+use rep2p::transport::{Connection, Listener};
+use rep2p::Switch;
+
+/// The `dnsaddr` transport implementation.
+pub struct DnsAddr;
+
+#[allow(unused)]
+#[async_trait]
+impl DriverTransport for DnsAddr {
+    /// Create a server-side socket with provided [`laddr`](Multiaddr).
+    async fn bind(&self, laddr: &Multiaddr, switch: Switch) -> Result<Listener> {
+        todo!()
+    }
+
+    /// Connect to peer with remote peer [`raddr`](Multiaddr).
+    async fn connect(&self, raddr: &Multiaddr, switch: Switch) -> Result<Connection> {
+        todo!()
+    }
+
+    /// Check if this transport support the protocol stack represented by the `addr`.
+    fn multiaddr_hit(&self, addr: &Multiaddr) -> bool {
+        todo!()
+    }
+}
